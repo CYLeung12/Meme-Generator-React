53cf5dceb8ec25d443b41ae2cd841718ca863db8
lcy <cyleung1212@gmail.com>
2023-09-14 21:24:27 +0100

    add random image function

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = useState("");
+
+    const getMemeImage = () => {
+        const Memes = memesData.data.memes;
+        const randomMemeItem = Memes[Math.floor(Math.random() * Memes.length)];
+        setMemeImage(randomMemeItem.url.toString());
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top Text" />
                 <input type="text" className="form--input" placeholder="Bottom Text" />
-                <button className="form--button">Get your Meme!</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get your Meme!</button>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     )
 }
